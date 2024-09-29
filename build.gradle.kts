635424f6f3b277010855e4d66cb7278fe6e564a8
Sercan Yiğit <sercnyigit@gmail.com>
2024-09-29 14:52:41 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.kotlinKsp) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
