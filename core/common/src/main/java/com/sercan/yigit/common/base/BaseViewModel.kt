e63380ff2a4868ec26913a77e8f2f7340363a516
Sercan Yiğit <sercnyigit@gmail.com>
2024-10-04 17:50:31 +0300

    [sercan.yigit][refactor connectivityManager]

--- core/common/src/main/java/com/sercan/yigit/common/base/BaseViewModel.kt
+++ core/common/src/main/java/com/sercan/yigit/common/base/BaseViewModel.kt
@@ -5,30 +5,44 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 open class BaseViewModel(context: Context) : ViewModel() {
-    private val connectivityManager = getSystemService(context, ConnectivityManager::class.java)
 
-    private val _isConnected = MutableStateFlow<Boolean>(false)
+    private val connectivityManager by lazy {
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    private val _isConnected = MutableStateFlow(false)
     val isConnected: StateFlow<Boolean> = _isConnected
 
     init {
+        observeNetworkConnectivity()
+    }
+
+    private fun observeNetworkConnectivity() {
         val networkRequest = NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .build()
 
-        connectivityManager?.registerNetworkCallback(networkRequest, object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                _isConnected.value = true
-            }
+        connectivityManager.registerNetworkCallback(
+            networkRequest,
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    _isConnected.value = true
+                }
 
-            override fun onLost(network: Network) {
-                _isConnected.value = false
+                override fun onLost(network: Network) {
+                    _isConnected.value = false
+                }
             }
-        })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        connectivityManager.unregisterNetworkCallback(ConnectivityManager.NetworkCallback())
     }
 }
--- core/common/src/main/java/com/sercan/yigit/common/base/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.sercan.yigit.common.base
-
-sealed class UiState<out T> {
-    object Loading : UiState<Nothing>()
-    data class Success<out T>(val data: T) : UiState<T>()
-    data class Error(val message: String) : UiState<Nothing>()
-}
